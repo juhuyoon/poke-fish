adbc147249dd7b28909b6e1265feb021c40348d1
jyoon3 <jyoon3@2u.com>
2024-09-12 21:58:23 -0400

    react router and apollo connected

---
+++ client/src/components/Food.jsx
@@ -0,0 +1,9 @@
+const Food = () => {
+  return (
+    <div>
+      Food page!
+    </div>
+  )
+}
+
+export default Food;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home.jsx';
+import Food from './components/Food.jsx';
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/food",
+        element: <Food />
+      }
+    ]
+  }
+]);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
